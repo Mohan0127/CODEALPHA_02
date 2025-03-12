cd196617e419f8e2d09897c6e34ea82e42d4b76b
Mohan0127 <raajmohan542@gmail.com>
2025-03-12 18:20:06 +0530

    Initial commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Display from "./Display";
+import Button from "./Button";
+
+const Calculator = () => {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    if (value === "=") {
+      try {
+        setInput(eval(input).toString());
+      } catch {
+        setInput("Error");
+      }
+    } else if (value === "C") {
+      setInput("");
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <Display value={input} />
+      <div className="buttons">
+        {[
+          "7",
+          "8",
+          "9",
+          "/",
+          "4",
+          "5",
+          "6",
+          "*",
+          "1",
+          "2",
+          "3",
+          "-",
+          "0",
+          ".",
+          "=",
+          "+",
+        ].map((btn) => (
+          <Button key={btn} label={btn} onClick={() => handleClick(btn)} />
+        ))}
+        <Button label="C" onClick={() => handleClick("C")} className="clear" />
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
---
+++ src/components/Display.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Display = ({ value }) => {
+  return <div className="display">{value || "0"}</div>;
+};
+
+export default Display;
